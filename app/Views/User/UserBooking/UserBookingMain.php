fff13887a1fbb5f4a7a94f3bc3f80568bf08bc4f
dekpiano <dekpiano4815@gmail.com>
2023-11-17 18:44:04 +0700

    17/11/2566 รอทำ อีเมล

--- app/Views/User/UserBooking/UserBookingMain.php
+++ app/Views/User/UserBooking/UserBookingMain.php
@@ -16,43 +16,42 @@
             <div class="row">
                 <div class="col-sm-6 col-lg-3 mb-4">
                     <a href="<?=base_url('Booking/Select')?>">
-                        <div class="card h-100 bg-warning">
+                        <div class="card h-100 bg-primary text-white">
                             <div class="card-body">
                                 <div class="d-flex align-items-center mb-2 pb-1">
                                     <div class="avatar me-2">
-                                        <span class="avatar-initial rounded bg-label-warning"><i
-                                                class="bx bx-error"></i></span>
+                                        <span class="avatar-initial rounded bg-label-info"><i class='bx bx-add-to-queue'></i></span>
                                     </div>
-                                    <h4 class="ms-1 mb-0"><?=$CountLocationRoomAll;?></h4>
+                                    <h4 class="ms-1 mb-0  text-white"><?=$CountLocationRoomAll;?></h4>
                                 </div>
-                                <p class="mb-1 h5">เลือกห้องประชุม / สถานที่</p>
+                                <p class="mb-1 h5 text-white">เลือกห้องประชุม / สถานที่</p>
                             </div>
                         </div>
                     </a>
                 </div>
                 <div class="col-sm-6 col-lg-3 mb-4">
                     <a href="<?=base_url('Booking/View/All')?>">
-                        <div class="card h-100 bg-primary text-white">
+                        <div class="card h-100 bg-warning">
                             <div class="card-body ">
                                 <div class="d-flex align-items-center mb-2 pb-1">
                                     <div class="avatar me-2">
-                                        <span class="avatar-initial rounded bg-label-info"><i
+                                        <span class="avatar-initial rounded  bg-label-warning"><i
                                                 class="bx bx-time-five"></i></span>
                                     </div>
-                                    <h4 class="ms-1 mb-0 text-white"><?=$CountbookingAll;?> รายการ</h4>
+                                    <h4 class="ms-1 mb-0"><?=$CountbookingAll;?> รายการ</h4>
                                 </div>
-                                <p class="mb-1 h5 text-white">สถานะจองห้องประชุม / สถานที่</p>
+                                <p class="mb-1 h5">สถานะจองห้องประชุม / สถานที่</p>
                             </div>
                         </div>
                     </a>
                 </div>
 
 
-                <?php if(isset($_SESSION['username']) && $_SESSION['status'] =="AdminGeneral" || $_SESSION['status'] =="ExecutiveGeneral"):?>
+                <?php if(isset($_SESSION['username']) && @$_SESSION['status'] =="AdminGeneral" || @$_SESSION['status'] =="ExecutiveGeneral"):?>
                 <div class="col-sm-6 col-lg-3 mb-4 offset-md-3">
-                    <?php if($_SESSION['status'] =="AdminGeneral"):?>
+                    <?php if(@$_SESSION['status'] =="AdminGeneral"):?>
                     <a href="<?=base_url('Booking/Approve/Admin')?>">
-                    <?php elseif($_SESSION['status'] =="ExecutiveGeneral"): ?>
+                    <?php elseif(@$_SESSION['status'] =="ExecutiveGeneral"): ?>
                         <a href="<?=base_url('Booking/Approve/Executive')?>">
                     <?php endif;?>
                         <div class="card h-100 bg-info text-white">
